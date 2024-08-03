1c2be65e99795b98e263a6c3aff719e586ff9e25
Satoki Hata <satokihata@gmail.com>
2024-08-03 17:32:23 +0900

    9.テストのドキュメント化

--- src/test.test.ts
+++ src/test.test.ts
@@ -1,20 +1,22 @@
 import { convert } from "./test";
 
-describe("convert test", () => {
-  test("1を渡すと文字列1を返す", () => {
-    // 実行 & 検証
-    expect(convert(1)).toBe("1");
+describe("convert関数は数を文字列に変換する", () => {
+  describe("3 の倍数の時は数の代わりに「Fizz」", () => {
+    test("3を渡すと文字列'Fizz'を返す", () => {
+      // 実行 & 検証
+      expect(convert(3)).toBe("Fizz");
+    });
   });
-  test("2を渡すと文字列2を返す", () => {
-    // 実行 & 検証
-    expect(convert(2)).toBe("2");
+  describe("5 の倍数の時は数の代わりに「Buzz」", () => {
+    test("5を渡すと文字列'Buzz'を返す", () => {
+      // 実行 & 検証
+      expect(convert(5)).toBe("Buzz");
+    });
   });
-  test("3を渡すと文字列'Fizz'を返す", () => {
-    // 実行 & 検証
-    expect(convert(3)).toBe("Fizz");
-  });
-  test("5を渡すと文字列'Buzz'を返す", () => {
-    // 実行 & 検証
-    expect(convert(5)).toBe("Buzz");
+  describe("その他の数の場合はそのまま文字列に変換する", () => {
+    test("1を渡すと文字列1を返す", () => {
+      // 実行 & 検証
+      expect(convert(1)).toBe("1");
+    });
   });
 });
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 export const convert = (num: number): string => {
   if (num % 3 === 0) return "Fizz";
-  if (num % 5 === 0) return "Fizz";
+  if (num % 5 === 0) return "Buzz";
   return String(num);
 };
